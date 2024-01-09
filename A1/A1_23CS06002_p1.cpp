a511e9c2efbb78cefd4f09b4b68e07476114ad91
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-01-09 14:31:19 +0000

    question 1 drver code

--- A1/A1_23CS06002_p1.cpp
+++ A1/A1_23CS06002_p1.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+double radius = 0;
+
+
 class Solution{
     public:
 
@@ -8,5 +11,19 @@ class Solution{
 
 int main(){
     Solution sol;
+    vector<vector<int>> rectangle;
+    int n,m =2;
+    cout << "Enter number of points; ";
+    cin >>n;
+    for(int i=0;i<n;i++){
+        vector<int> temp;
+        for(int j=0;j<m;j++){
+            int a;
+            cin>>a;
+            temp.push_back(a);
+        }
+        rectangle.push_back(temp);
+    }
+
     return 0;
 }
