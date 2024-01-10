55747e87e02a94a47cef78f731f73bc820d6654b
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-01-10 16:01:36 +0000

    doing something

--- A1/A1_23CS06002_p1.cpp
+++ A1/A1_23CS06002_p1.cpp
@@ -1,29 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-double radius = 0;
+const double INF = 1e18;
 
 
 class Solution{
+    double radius = 0;
+    double distance =0;
     public:
-
-};
-
-int main(){
-    Solution sol;
-    vector<vector<int>> rectangle;
-    int n,m =2;
-    cout << "Enter number of points; ";
-    cin >>n;
-    for(int i=0;i<n;i++){
-        vector<int> temp;
-        for(int j=0;j<m;j++){
-            int a;
-            cin>>a;
-            temp.push_back(a);
-        }
-        rectangle.push_back(temp);
+    double dis(pair<double,double>p1,pair<double,double>p2){
+        return sqrt(pow(p1.first-p2.second ,2) + pow(p1.second-p2.second,2));
     }
 
+    pair<double,double>circle_from(pair<double,double>p1,pair<double,double>p2){
+        pair<double,double>c;
+        c = { (p1.first + p2.second) /2 ,(p1.second + p2.second)/2};
+        distance = dis(p1,p2);
+        return c;
+
+    }
+
+
+    pair<double,double> minimumcircle(vector<pair<double,double>>&points){
+        pair<double ,double> center;
+        double n= points.size();
+        if(n==0)return {0,0};
+        if(n==1) return{points[0].first , points[0].second};
+
+        for(double i=0;i<n;i++){
+            for(double j=i+1;j<n;j++){
+                pair<double,double> temp;
+                temp = circle_from(points[i],points[j]);
+            }
+        }
+
+
+
+        return center;
+    }
+};
+
+double main(){
+    Solution sol;
+    vector<pair<double,double >> points;
+    double n;
+    cout << "Enter number of podoubles; ";
+    cin >>n;
+    pair<double,double> temp;
+    for(double i=0;i<2;i++){
+        double a,b;
+        cin >> a >>b;
+        temp.first = a;
+        temp.second =b;
+        points.push_back(temp);
+    }
+    temp = sol.minimumcircle(points);
+    cout << "Center is " << "{" << temp.first << "," << temp.second <<"}"<< endl;
     return 0;
 }
