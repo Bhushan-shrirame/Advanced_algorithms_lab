e6576f6327a8ded661a051d67bd256640d4e1a8d
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-01-12 13:43:05 +0000

    question 2 complete

--- A2/A2_23CS06002_p2.cpp
+++ A2/A2_23CS06002_p2.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 class Solution{
     public:
+    int countstringwithvowel(int n){
+        n++;
+        cout << (n+3)*(n+2)*(n+1)*n/24 << endl;
+		return (n+3)*(n+2)*(n+1)*n/24;
+    }
 };
 
 int main(){
@@ -10,5 +15,8 @@ int main(){
     cin.tie(0);
     cout.tie(0);
     Solution sol;
+    int n;
+    cin >> n;
+    sol.countstringwithvowel(n);
     return 0;
 }
