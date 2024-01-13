6ac34155e409c85c890d495468dc390e02599ae7
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-01-13 07:56:21 +0000

    .

--- A2/A2_23CS06002_p3.cpp
+++ A2/A2_23CS06002_p3.cpp
@@ -10,5 +10,19 @@ int main(){
     cin.tie(0);
     cout.tie(0);
     Solution sol;
+    int n,e;
+    cout << "Enter no. of nodes ";
+    cin >> n; 
+    cout << "Enter no. of edge ";
+    cin >> e;
+    vector<vector<int>>edges;
+    for(int i=0;i<e;i++){
+        int a , b;
+        cin >> a ,b;
+        edges.push_back({a,b});
+    }
+    for(int i=0;i<e;i++){
+        cout << edges[i][0] << " " << edges[i][1] << endl;
+    }
     return 0;
 }
