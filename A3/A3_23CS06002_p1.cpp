653b7f3e1bab40a22c2b92165d6415c3292bc3a3
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-01-23 11:49:30 +0000

    .

--- A3/A3_23CS06002_p1.cpp
+++ A3/A3_23CS06002_p1.cpp
@@ -5,8 +5,6 @@ class Solution{
 	public:
     unordered_set<string> seen;
     vector<int> edges;
-    // Modified DFS in which no edge
-    // is traversed twice
     void dfs(string node, int& k, string& A)
     {
     	for (int i = 0; i < k; ++i) {
@@ -18,26 +16,18 @@ class Solution{
     		}
     	}
     }
-    
-    // Function to find a de Bruijn sequence
-    // of order n on k characters
+
     string deBruijn(int n, int k, string A)
     {
-    
-    	// Clearing global variables
     	seen.clear();
     	edges.clear();
-    
     	string startingNode = string(n - 1, A[0]);
     	dfs(startingNode, k, A);
-    
     	string S;
-    
-    	// Number of edges
     	int l = pow(k, n);
-    	for (int i = 0; i < l; ++i)
-    		S += A[edges[i]];
+    	for (int i = 0; i < l; ++i) S += A[edges[i]];
     	S += startingNode;
+        cout << S <<endl;
     
     	return S;
     }
