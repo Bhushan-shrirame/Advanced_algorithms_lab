4806e4ba1788475159a5cfd4fdaad410b264ba97
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-02-08 18:46:45 +0000

    .

--- A4/A4_23CS06002.cpp
+++ A4/A4_23CS06002.cpp
@@ -3,25 +3,29 @@ using namespace std;
 
 class Solution{
 public:
-    // this vector will store the graph and the first bool value will store if is is used or not;
     vector<pair<bool,pair<int,int>>> graph;
-
-
-    //to check whether all the neighbour is used or not
-    bool all_neighbour_used(int q){
-        //TODO
+    bool all_neighbour_used(int q,int p){
+        for(auto i:graph){
+            if(i.second.first == p && i.first == false){
+                return false;
+            }
+        }
+        return true;
     }
-
-    //to check if their is some neighbour where (q != father[p]) or (!used[p][q])
     int some_neighbour_not_used(int q, int p ,vector<int>father){
-        //TODO
+        int x;
+        for(auto i:graph){
+            if(i.second.first == p){
+                if(i.second.second != father[p] || !i.first){
+                    x = i.second.second;
+                }
+            }
+        }
+        return x;
     }
-
-    // solver function
     void Solver(vector<pair<int ,int>> grp ){
         vector<int>father(6 ,-1);
         int p,q0,q;
-        //updating the graph
         int j=0;
         for(auto i:grp){
             graph[j].second.first = i.first;
@@ -29,55 +33,49 @@ public:
             graph[j].first = false;
             j++;
         }
-
-        // Starting algorithm
-
-        // initializing the initiator
-        int p = graph[0].second.first;
+        p = graph[0].second.first;
         cout<< "initiator"<<p << "\n";
         father[p] = p;
         q = p;
-        int p = graph[0].second.second;
+        p = graph[0].second.second;
         graph[0].first = true;
         q0=q;
-
-        //wheather the channel p->q has been used;
         while(p){
             if(father[p] == -1) father[p] = q0;
-            if(all_neighbour_used(q)){
+            if(all_neighbour_used(q ,p)){
                 break;
             }
             else if(some_neighbour_not_used(q,p,father)){
                 if((father[p] !=q) ) q = q0;   
                 else q = some_neighbour_not_used(q,p,father);
                 for(auto i: graph)if(i.second.first == p && i.second.second == q) i.first = true;
-                // father[p] = q; 
+                father[p] = q; 
             }
             else {
                 for(auto i: graph) if(i.second.first == p && i.second.second == father[p]) i.first = true;
-                // father[p] = father[p];
+                father[p] = father[p];
             }
         }
 
         print(father);
     }
-
-    // printing father and son;
     void print(vector<int>father){
         for(int i=0;i<father.size();i++)cout << "Father" << father[i] << "son" << i << "\n";
     }
 };
 
 int main(){
-    // ios_base::sync_with_stdio(false);
-	// cin.tie(0);
-	// cout.tie(0);
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
     Solution st;
-    int n;
+    int nodes ,edges;
     // cin >> n;
-    n=6;
+    nodes=6;
+    cout << "Enter number of edges ";
+    cin >> edges;
     vector<pair<int ,int>> grp(0);
-    for(int i=0;i<n;i++){
+    for(int i=0;i<edges;i++){
         int a ,b;
         cin >> a >> b;
         grp.push_back({a,b});
