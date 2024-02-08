c3ed7439046baf765c05fc02d239ab15bcf6ce84
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-02-08 17:05:23 +0000

    .

--- A4/A4_23CS06002.cpp
+++ A4/A4_23CS06002.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// no. of nodes
+extern int global;
+
 class Solution{
 public:
     // this vector will store the graph and the first bool value will store if is is used or not;
     vector<pair<bool,pair<int,int>>> graph;
-    vector<pair<int ,int>> father;
-    void Solver(vector<pair<int ,int>> grp){
+    void Solver(vector<pair<int ,int>> grp ){
+        vector<int>father(6 ,-1);
 
         //updating the graph
         int j=0;
@@ -18,16 +21,18 @@ public:
         }
 
         // starting algorithm
+        int initiator = graph[0].second.first;
+        cout<< "initiator"<<initiator << "\n";
 
     }
 };
 
 int main(){
     Solution st;
-    int n;
-    cin >> n;
+    cin >> global;
+    
     vector<pair<int ,int>> grp(0);
-    for(int i=0;i<n;i++){
+    for(int i=0;i<global;i++){
         int a ,b;
         cin >> a >> b;
         grp.push_back({a,b});
