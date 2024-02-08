8412143c7939dd7783769372d39f8768fd7fc6b6
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-02-08 17:12:44 +0000

    .

--- A4/A4_23CS06002.cpp
+++ A4/A4_23CS06002.cpp
@@ -10,7 +10,7 @@ public:
     vector<pair<bool,pair<int,int>>> graph;
     void Solver(vector<pair<int ,int>> grp ){
         vector<int>father(6 ,-1);
-
+        int p,q0;
         //updating the graph
         int j=0;
         for(auto i:grp){
@@ -21,8 +21,20 @@ public:
         }
 
         // starting algorithm
+
+        //this will run one time so no need for loop;
+        // initializing the initiator
+
         int initiator = graph[0].second.first;
         cout<< "initiator"<<initiator << "\n";
+        father[initiator] = initiator;
+        cout << "Father" << initiator << "son " << initiator << "\n";
+        int choose = graph[0].second.second;
+        graph[0].first = true;
+
+
+
+        //wheather the channel p->q has been used;
 
     }
 };
