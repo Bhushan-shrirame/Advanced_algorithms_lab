727276995d21dd87435a72042378d5a676383127
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-02-12 16:57:39 +0000

    completed

--- A5/A5_23CS06002_p3.cpp
+++ A5/A5_23CS06002_p3.cpp
@@ -3,9 +3,60 @@ using namespace std;
 
 class Solution {
 public:
-
-};
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;        
+        int n = heights.size();
+        int maxa = -1 ;
+        int left_small[n] ;
+        int right_small[n] ;
+    
+        for(int i = 0 ; i < n ; i++){  
+            if(st.empty()){
+                st.push(i);
+                left_small[i] = 0;
+            } 
+            if( (!st.empty()) && (heights[st.top()] <= heights[i])){
+                left_small[i] = st.top() + 1;
+                st.push(i);   
+            }
+            while((!st.empty()) && (heights[st.top()] >= heights[i])){
+                st.pop();
+                if( (!st.empty()) && (heights[st.top()] <= heights[i])){
+                    left_small[i] = st.top() + 1; 
+                }
+            }
+            if(st.empty()){
+                    left_small[i] = 0;
+            }
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        
+        for(int i = n-1 ; i >= 0 ; i--){   
+            while((!st.empty()) && (heights[st.top()] >= heights[i])) st.pop();
+            if(st.empty())right_small[i] = n-1;
+            else right_small[i] = (st.top() - 1);   
+            st.push(i);
+        }
+        for(int x = 0 ; x < n ; x++) maxa =  max(maxa,(heights[x] * ( right_small[x] - left_small[x] + 1 )));
+        return maxa;
+    }
+};      
 
 int main(){
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+    Solution st;
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for(int i=0;i<n;i++){
+        int a;
+        cin >> a;
+        nums.push_back(a);
+    }
+    int res = st.largestRectangleArea(nums);
+    cout << "Result: " << res << endl;
     return 0;
 }
--- A5/A5_23CS06002_p4.cpp
+++ A5/A5_23CS06002_p4.cpp
@@ -3,9 +3,31 @@ using namespace std;
 
 class Solution {
 public:
-
-};
+    int trap(vector<int>& h) {
+        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
+        while(l<r){
+            lmax=max(lmax,h[l]);
+            rmax=max(rmax,h[r]);
+            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
+        }
+        return ans;
+    }
+};    
 
 int main(){
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+    Solution st;
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for(int i=0;i<n;i++){
+        int a;
+        cin >> a;
+        nums.push_back(a);
+    }
+    int res = st.trap(nums);
+    cout << "Result: " << res << endl;
     return 0;
 }
