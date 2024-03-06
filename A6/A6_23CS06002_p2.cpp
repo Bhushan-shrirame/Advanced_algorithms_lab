3a275402c2a760f8e7ff03604c63ac87dd2d3dea
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-03-06 16:14:55 +0000

    .

--- A6/A6_23CS06002_p2.cpp
+++ A6/A6_23CS06002_p2.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 class Solution{
 public:
-    void Egg_Dropping_Puzzle(){
-
+    int Egg_Dropping_Puzzle(int n, int k){
+        vector<vector<int> > dp(k + 1, vector<int>(n + 1, 0));
+        int m = 0;
+        while (dp[m][n] < k) {
+            m++;
+            for (int x = 1; x <= n; x++) dp[m][x] = 1 + dp[m - 1][x - 1] + dp[m - 1][x];
+        }
+        return m;
     }
 };
 
@@ -13,6 +19,8 @@ int main(){
 	cin.tie(0);
 	cout.tie(0);
     Solution st;
-    st.Egg_Dropping_Puzzle();
+    int n,k;
+    cin >> n >> k;
+    cout << st.Egg_Dropping_Puzzle(n,k) << endl;
     return 0;
 }
---
+++ A6/A6_23CS06002_p3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    void Minimum_Concrete_for_Staircase_Reduction(){
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+    Solution st;
+    int n,k;
+    cin >> n >> k;
+    st.Minimum_Concrete_for_Staircase_Reduction();
+    return 0;
+}
