6d500496178556c95cb1224848d3b5c299c94fc2
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-03-06 16:28:49 +0000

    .

--- A6/A6_23CS06002_p3.cpp
+++ A6/A6_23CS06002_p3.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 class Solution{
 public:
-    void Minimum_Concrete_for_Staircase_Reduction(){
+    int  Minimum_Concrete_for_Staircase_Reduction(int k,int m,vector<vector<int>>&steps){
+        int res =0;
+        return res;
     }
 };
 
@@ -12,8 +14,14 @@ int main(){
 	cin.tie(0);
 	cout.tie(0);
     Solution st;
-    int n,k;
-    cin >> n >> k;
-    st.Minimum_Concrete_for_Staircase_Reduction();
+    int k,m;
+    cin >> k >> m;
+    vector<vector<int>>steps;
+    for(int i=0;i<k;i++){
+        int l ,h;
+        cin >> l >> h;
+        steps.push_back({l,h});
+    }
+    cout << st.Minimum_Concrete_for_Staircase_Reduction(k,m,steps) << endl;
     return 0;
 }
