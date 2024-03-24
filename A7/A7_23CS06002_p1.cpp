e391d21e671c758f2f85617dead6f4323ea7f4ae
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-03-24 10:59:29 +0000

    p1  done

--- A7/A7_23CS06002_p1.cpp
+++ A7/A7_23CS06002_p1.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 class Solution{
 public:
-    void Solver(){
-
+    int Weighted_Interval_Scheduling(vector<vector<int>> jobs){
+        int n = jobs.size();
+        sort(jobs.begin(),jobs.end());
+        vector<int> dp(n + 1, 0);
+        for (int idx = n - 1; idx >= 0; idx--) {
+            int notTake = dp[idx + 1];
+            vector<int> v = {jobs[idx][1], 0, 0};
+            int nextIdx = lower_bound(jobs.begin(), jobs.end(), v) - jobs.begin();
+            int take = jobs[idx][2] + dp[nextIdx];
+            dp[idx] = max(take, notTake);
+        }
+        return dp[0];
     }
 };
 
@@ -12,6 +22,16 @@ int main(){
 	cin.tie(0);
 	cout.tie(0);
     Solution st;
-    st.Solver();
+    int n;
+    cin >> n;
+    vector<vector<int>> jobs;
+    for(int i=0;i<n;i++){
+        int a , b ,c;
+        cin >> a >> b >> c;
+        jobs.push_back({a,b,c});
+
+    }
+    int res  = st.Weighted_Interval_Scheduling(jobs);
+    cout << "Result: " << res <<endl;
     return 0;
 }
