bc44e7c18b6c8d875d33e54bc7034489feea32d7
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-03-24 11:10:36 +0000

    p2complete

--- A7/A7_23CS06002_p2.cpp
+++ A7/A7_23CS06002_p2.cpp
@@ -1,9 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution{
+class Solution {
 public:
-    int  Maximum_Number_of_Envelops(vector<vector<int>> Envelops){
-        return 0;
+    static bool comp(pair<int,int> &a,pair<int,int>&b){
+        if(a.first==b.first){
+            return a.second>b.second;
+        }
+        else{
+            return a.first<b.first;
+        }
+    }
+
+    int Maximum_Number_of_Envelops(vector<vector<int>>& env) {
+        vector<pair<int,int>>arr(env.size());
+        for(int i = 0 ; i < env.size() ; i++){
+            arr[i] = {env[i][0],env[i][1]};
+        }
+
+        sort(arr.begin(),arr.end(),comp);
+        
+        vector<int>dp(env.size());
+        int ans = 0;
+
+        for(int i = 0 ; i < env.size() ; i++)
+        {
+            int l = 0;
+            int h = ans;
+            while(l<h){
+                int m = l +(h-l)/2;
+
+                if(dp[m] < arr[i].second) l = m + 1;
+                else h = m;
+            }
+
+            dp[l] = arr[i].second;
+            if(l == ans) ans++;
+        }
+        return ans;
     }
 };
 
