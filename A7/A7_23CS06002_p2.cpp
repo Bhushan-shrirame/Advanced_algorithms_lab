6b22ef03609082993ef1c11b78cf1b1f03aec988
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2024-03-24 11:07:00 +0000

    .

--- A7/A7_23CS06002_p2.cpp
+++ A7/A7_23CS06002_p2.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 class Solution{
 public:
-    void Solver(){
-
+    int  Maximum_Number_of_Envelops(vector<vector<int>> Envelops){
+        return 0;
     }
 };
 
@@ -12,6 +12,15 @@ int main(){
 	cin.tie(0);
 	cout.tie(0);
     Solution st;
-    st.Solver();
+    int n;
+    cin >> n;
+    vector<vector<int>> Envelops;
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin >> a >> b;
+        Envelops.push_back({a,b});
+    }
+    st.Maximum_Number_of_Envelops(Envelops);
     return 0;
 }
+
